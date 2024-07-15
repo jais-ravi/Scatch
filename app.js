cb0bf1576f6c1fd2a5c17ed61069b73735807285
jais-ravi <ravijaiswal258852@gmail.com>
2024-07-15 19:44:22 +0530

    added sepration of concers

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const express = require('express');
-const express = require('express');
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const ownersRouter = require("./routes/ownersRouters")
+const usersRouter = require("./routes/usersRouters")
+const productsRouter = require("./routes/productsRouters")
+
+const db = require("./config/mongoose-connection")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs");
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+app.listen(3000);
