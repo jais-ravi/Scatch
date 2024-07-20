42f81758e0298d0bdad7edf582e971b6a57246f9
jais-ravi <ravijaiswal258852@gmail.com>
2024-07-20 11:00:11 +0530

    done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const isloggedin = require("../middlewares/isLoggedIn");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/", function (req, res) {
     let error = req.flash("error");
@@ -10,14 +11,27 @@ router.get("/", function (req, res) {
 
 router.get("/shop", isloggedin, async function (req, res) {
     let products = await productModel.find();
-    res.render("shop", { products });
+    let success = req.flash("success");
+    res.render("shop", { products, success });
+});
+router.get("/cart", isloggedin, async function (req, res) {
+    let user = await userModel
+        .findOne({ email: req.user.email })
+        .populate("cart");
+
+    const bill = Number(user.cart[0].price) + 20 - Number(user.cart[0].discount);
+    res.render("cart", { user, bill });
 });
 
-router.get("/addtocart/:id", isloggedin, async function (req, res) {
-    
+router.get("/addtocart/:productid", isloggedin, async function (req, res) {
+    let user = await userModel.findOne({ email: req.user.email });
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success", "Added to cart");
+    res.redirect("/shop");
 });
 
-router.get("/logout",isloggedin,function(req,res){
+router.get("/logout", isloggedin, function (req, res) {
     res.redirect("/");
 })
 
